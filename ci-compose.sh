f2950faf70673b6158f9633d8354b0325eb0a34c
Manas Kocharekar <kocharekarmanas@gmail.com>
2024-01-09 22:46:10 +0530

    added comments

--- ci-compose.sh
+++ ci-compose.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# go to www 
 cd /var/www/
+
+# run composer command in the new folder
 composer create-project codeigniter4/appstarter $1
 
+#copy files from public folder to root
 cp $1/public/index.php $1/index.php
-
 cp $1/public/.htaccess $1/.htaccess
 
+# correct the path in index.php file
 sed -i 's/..\/app/\/app/g' $1/index.php
