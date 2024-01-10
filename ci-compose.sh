d9a1824c6ab2e4a55eb529053757785b67010190
Manas Kocharekar <kocharekarmanas@gmail.com>
2024-01-10 12:20:36 +0530

    context for /public/ folder

--- ci-compose.sh
+++ ci-compose.sh
@@ -6,7 +6,9 @@ cd /var/www/
 # run composer command in the new folder
 composer create-project codeigniter4/appstarter $1
 
-#copy files from public folder to root
+# copy files from public folder to root
+# this is not recommeneded by CI, it's only for convinience, 
+# skip these two and edit the index.php file in the public folder for the next step
 cp $1/public/index.php $1/index.php
 cp $1/public/.htaccess $1/.htaccess
 
